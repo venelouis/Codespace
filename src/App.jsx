fc579b05fdf258db5330901601e6411b4e08d6f6
@venelouis <37526748+venelouis@users.noreply.github.com>
2024-01-19 23:08:00 +0000

    codespace template deployed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Venelouis",
+  title: "Web Developer",
+  email: "venelouis1@gmail.com",
+  gitHub: "venelouis",
+  instagram: "venelouis",
+  linkedIn: "venelouis",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
